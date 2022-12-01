5613692cad893beaecad9a2c0b1185d12f679dd4
revenorr <93118055+revenorr@users.noreply.github.com>
2022-12-01 10:27:44 +0200

    21212

---
+++ Assets/UpBoss.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class UpBoss : MonoBehaviour
+{
+    public Enemy enemy;
+    public GameObject enemyTurel;
+    // Update is called once per frame
+    void Update()
+    {
+        enemy = GetComponent<Enemy>();
+        if (enemy.health < enemy.maxHealth*50/100)
+        {   
+            
+            enemyTurel.SetActive(true);
+        }
+    }
+}
